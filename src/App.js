807e3de5ecb7b3ddb574accfb408b1f6cd166eba
katietyndall <k.tyndall@yahoo.com>
2019-06-25 14:34:42 -0500

    added navigation bar

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodosList from "./components/todos-list.component";
+
+import logo from "./logo.png"
+
+function App() {
+  return (
+    <Router>
+    <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="codingthesmartway.com" target="_blank">
+          <img src={logo} width="50" height="50" alt="codingthesmartway.com" />
+        </a>
+        <Link to="/" className="navbar-brand">Moving-tool</Link>
+        <div className="navbar-collaspe">
+          <ul className="navbar-nav mr-auto">
+            <li className="navbar-item">
+              <Link to="/" className="nav-link">Todos</Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/create" className="nav-link">Create Todo</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+ 
+      <Route path="/" exact component={TodosList} />
+      <Route path="/edit/:id" component={EditTodo} />
+      <Route path="/create" component={CreateTodo} />
+    </div>
+    </Router>
+  );
+}
+
+export default App;
